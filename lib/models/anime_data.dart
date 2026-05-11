0444bff2e457c457fcbc10aa81635c83dba8e6c9
Шапкин Николай <kosmys122@gmail.com>
2026-05-11 19:36:48 +0300

    Add AnimeList with ListView.builder and connect to main

--- lib/models/anime_data.dart
+++ lib/models/anime_data.dart
@@ -7,7 +7,7 @@ const animeList = [
     genre: 'Триллер, Психология',
     year: 2006,
     rating: 9.0,
-    imagePath: 'assets/images/anime1.jpg',
+    imagePath: 'img/anime1.jpg',
   ),
   Anime(
     title: 'Attack on Titan',
@@ -15,7 +15,7 @@ const animeList = [
     genre: 'Экшен, Драма',
     year: 2013,
     rating: 9.1,
-    imagePath: 'assets/images/anime2.jpg'
+    imagePath: 'img/anime2.jpg'
   ),
   Anime(
     title: 'Naruto',
@@ -23,7 +23,7 @@ const animeList = [
     genre: 'Экшен, Приключения',
     year: 2002,
     rating: 8.3,
-    imagePath: 'assets/images/anime3.jpg'
+    imagePath: 'img/anime3.jpg'
   ),
   Anime( 
     title: 'One Piece',
@@ -31,47 +31,47 @@ const animeList = [
     genre: 'Приключения, Комедия',
     year: 1999,
     rating:8.9,
-    imagePath: 'assets/images/anime4.jpg'),
+    imagePath: 'img/anime4.jpg'),
   Anime( 
     title:'Demon Slayer',
     description:'Танджиро сражается с демонами, чтобы спасти свою сестру и отомстить за семью.',
     genre:'Экшен, Фэнтези',
     year:2019,
     rating:8.7,
-    imagePath: 'assets/images/anime5.jpg'),
+    imagePath: 'img/anime5.jpg'),
   Anime(
     title:'My Hero Academia',
     description:'В мире супергероев подросток без способностей мечтает стать величайшим героем.',
     genre:'Экшен, Супергерои',
     year:2016,
     rating:8.4,
-    imagePath: 'assets/images/anime6.jpg'),
+    imagePath: 'img/anime6.jpg'),
   Anime( 
     title:'Sword Art Online',
     description:'Игроки оказываются в ловушке виртуальной реальности, где смерть в игре означает смерть в реальности.',
     genre:'Фэнтези, Приключения',
     year:2012,
     rating:7.7,
-    imagePath: 'assets/images/anime7.jpg'),
+    imagePath: 'img/anime7.jpg'),
   Anime(
     title:'Fullmetal Alchemist: Brotherhood',
     description:'Два брата-алхимика ищут философский камень, чтобы вернуть утраченные тела.',
     genre:'Экшен, Фэнтези',
     year:2009,
     rating:9.1,
-    imagePath: 'assets/images/anime8.jpg'),
+    imagePath: 'img/anime8.jpg'),
   Anime(
     title:'Tokyo Ghoul',
     description:'Студент Канеки превращается в полу-гуля после трагического инцидента.',
     genre:'Ужасы, Экшен',
     year:2014,
     rating:7.9,
-    imagePath: 'assets/images/anime9.jpg'),
+    imagePath: 'img/anime9.jpg'),
   Anime(
     title:'Steins;Gate',
     description:'Учёный случайно изобретает машину времени и сталкивается с последствиями.',
     genre:'Sci-Fi, Триллер',
     year:2011,
     rating:9.1,
-    imagePath: 'assets/images/anime10.jpg'),
+    imagePath: 'img/anime10.jpg'),
 ];
